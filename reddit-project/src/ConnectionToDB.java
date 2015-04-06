4d846ba92b76e0f360ae6fe61a2f0420d7edc0aa
wiley.shi <wiley.shi@accenture.com>
2015-04-06 16:02:44 -0700

    stuff

--- reddit-project/src/ConnectionToDB.java
+++ reddit-project/src/ConnectionToDB.java
@@ -1,4 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//import org.apache.log4j.Logger;
 
 public class ConnectionToDB {
-
+    private static Connection connection = null;
+    
+    public static Connection createConnection() throws ClassNotFoundException,
+    SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "", "root", "root"); // enter db info
+        return connection;
+    }
 }
